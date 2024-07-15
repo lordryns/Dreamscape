8fc2217f13790ffbd7dd318530b3832c9699e505
David <rynstheoverlord@gmail.com>
2024-07-15 02:27:54 +0100

    Fixed static issue

--- website/auth.py
+++ website/auth.py
@@ -2,14 +2,14 @@ from flask import Blueprint, render_template, request
 
 auth = Blueprint( "auth", __name__)
 
-@auth.route('/signup', methods=['POST'])
+@auth.route('/signup', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
 
-        
+
 
         print(username, email, password)
     return render_template("register.html")
