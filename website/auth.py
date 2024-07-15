974a9a7016dee7bce0f502b3ed2863a1bf3e2818
David <rynstheoverlord@gmail.com>
2024-07-15 02:11:18 +0100

    Added appwrite as backend!

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 auth = Blueprint( "auth", __name__)
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['POST'])
 def register():
+    
     return render_template("register.html")
