fa67728e413f1c268ff852d784380770424a1aee
David <rynstheoverlord@gmail.com>
2024-07-15 02:14:57 +0100

    Listening for post requests

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint( "auth", __name__)
 
 @auth.route('/signup', methods=['POST'])
 def register():
-    
+    if request.method == 'POST':
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        print(username, email, password)
     return render_template("register.html")
